576843f9d28a3d18ab18ca3f11dca41aeb84ca4f
Aaron <aaronipollock@gmail.com>
2025-11-28 10:57:46 -0800

    Setup Local Development Environment

---
+++ bigLITTLEapp/app/index.jsx
@@ -0,0 +1,18 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
