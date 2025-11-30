1774a3f10f3631c8e337f190c970535ea79084d1
Aaron <aaronipollock@gmail.com>
2025-11-29 16:07:16 -0800

    Implement Initial Screen

--- bigLITTLEapp/app/index.tsx
+++ bigLITTLEapp/app/index.tsx
@@ -1,18 +1,53 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import { StatusBar } from "expo-status-bar";
 
-export default function HomeScreen() {
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from '@/components/CustomButton'
+
+const App = () => {
   return (
-    <View className="flex-1 justify-center">
-      <Text>Hello World</Text>
+    <View className='flex-1'>
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          style={styles.gradient}
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className='text-center text-white font-bold text-4xl'>
+                bigLITTLE
+              </Text>
+              <Text className='text-center text-white font-bold text-2xl mt-3'>
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton
+                onPress={() => console.log('tap')}
+                title='Get Started'
+              />
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
+  gradient: {
     flex: 1,
-    justifyContent: 'center',
-  }
-});
+  },
+})
+
+export default App
