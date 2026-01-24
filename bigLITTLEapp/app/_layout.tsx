9a47103879eaaa020991122ae921782b62e5f67b
Aaron <aaronipollock@gmail.com>
2026-01-23 16:36:38 -0800

    Meditation Details Screen

--- bigLITTLEapp/app/_layout.tsx
+++ bigLITTLEapp/app/_layout.tsx
@@ -1,14 +1,34 @@
 import "../global.css";
-import { Slot, Stack } from "expo-router";
+import { Slot, SplashScreen, Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+// This will prevent the splash screen from auto hiding until loading all the font assets
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
   return (
     <SafeAreaProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="meditate" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="meditate/[id]"
+          options={{ headerShown: false }}
+        />
       </Stack>
     </SafeAreaProvider>
   );
